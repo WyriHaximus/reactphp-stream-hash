1d3ac419fd9e03025f65a0c30706da8f2c55e265
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-07-06 08:09:19 +0200

    Initial code commit

---
+++ src/WritableStreamHash.php
@@ -0,0 +1,61 @@
+<?php declare(strict_types=1);
+
+namespace WyriHaximus\React\Stream\Hash;
+
+use Evenement\EventEmitterTrait;
+use React\Stream\WritableStreamInterface;
+
+final class WritableStreamHash implements WritableStreamInterface
+{
+    use EventEmitterTrait;
+
+    /**
+     * @var WritableStreamInterface
+     */
+    private $stream;
+
+    /**
+     * @var resource
+     */
+    private $context;
+
+    /**
+     * WritableStreamHash constructor.
+     * @param WritableStreamInterface $stream
+     */
+    public function __construct(WritableStreamInterface $stream, string $hash, int $options = 0, string $key = '')
+    {
+        $this->stream = $stream;
+        $this->context = hash_init($hash, $options, $key);
+        $this->stream->on('close', function () {
+            $this->emit('hash', [
+                hash_final($this->context),
+            ]);
+        });
+    }
+
+    public function isWritable()
+    {
+        return $this->stream->isWritable();
+    }
+
+    public function write($data)
+    {
+        hash_update($this->context, $data);
+
+        return $this->stream->write($data);
+    }
+
+    public function end($data = null)
+    {
+        if ($data !== null) {
+            hash_update($this->context, $data);
+        }
+        $this->stream->end($data);
+    }
+
+    public function close()
+    {
+        $this->stream->close();
+    }
+}
---
+++ tests/WritableStreamHashTest.php
@@ -0,0 +1,43 @@
+<?php declare(strict_types=1);
+
+namespace WyriHaximus\React\Tests\Stream\Hash;
+
+use PHPUnit\Framework\TestCase;
+use React\EventLoop\Factory;
+use React\Stream\ThroughStream;
+use WyriHaximus\React\Stream\Hash\WritableStreamHash;
+use function Clue\React\Block\await;
+use function React\Promise\Stream\buffer;
+
+final class WritableStreamHashTest extends TestCase
+{
+    public function provideData()
+    {
+        foreach (hash_algos() as $algo) {
+            yield [$algo, 'a'];
+            yield [$algo, 'abc'];
+            yield [$algo, str_pad('a', 1337)];
+            yield [$algo, str_pad('a', 100000)];
+        }
+    }
+
+    /**
+     * @dataProvider provideData
+     */
+    public function testHash(string $algo, string $data)
+    {
+        $catchedHash = null;
+        $loop = Factory::create();
+        $throughStream = new ThroughStream();
+        $stream = new WritableStreamHash($throughStream, $algo);
+        $stream->on('hash', function ($hash) use (&$catchedHash) {
+            $catchedHash = $hash;
+        });
+        $loop->futureTick(function () use ($stream, $data) {
+            $stream->write($data);
+            $stream->end();
+        });
+        self::assertSame($data, await(buffer($throughStream), $loop));
+        self::assertSame(hash($algo, $data), $catchedHash);
+    }
+}
