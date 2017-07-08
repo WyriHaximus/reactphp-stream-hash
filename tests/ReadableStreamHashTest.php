9ab03ae90d2f4de85e348f2aa5759f4ba227f04e
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-07-08 19:48:55 +0200

    Test against more data sets

--- tests/ReadableStreamHashTest.php
+++ tests/ReadableStreamHashTest.php
@@ -16,8 +16,17 @@ final class ReadableStreamHashTest extends TestCase
         foreach (hash_algos() as $algo) {
             yield [$algo, 'a'];
             yield [$algo, 'abc'];
+            yield [$algo, 'abcdefg'];
+            yield [$algo, 'abcdefghij'];
+            yield [$algo, 'abcdefghijklm'];
+            yield [$algo, 'abcdefghijklmnop'];
+            yield [$algo, 'abcdefghijklmnopqrst'];
+            yield [$algo, 'abcdefghijklmnopqrstuvw'];
+            yield [$algo, 'abcdefghijklmnopqrstuvwxyz'];
+            foreach (range(128, 256) as $size) {
+                yield [$algo, str_pad('a', $size)];
+            }
             yield [$algo, str_pad('a', 1337)];
-            yield [$algo, str_pad('a', 100000)];
         }
     }
 
